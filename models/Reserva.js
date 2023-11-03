2328735dcd1cd641bec9e100ef2749e25f264033
jse2021 <maldonadojose201422@gmail.com>
2023-11-03 16:56:12 -0300

    algunas modificaciones

--- models/Reserva.js
+++ models/Reserva.js
@@ -3,71 +3,81 @@ const {Schema, model} = require('mongoose')
 
 
 const ReservaSchema = Schema({
-  cliente: {
-      type: String,
-      required: true,
-      validate: {
-        // La regla de validación se usa para comprobar que el dni es válido
-        // Puede utilizar una expresión regular para comprobar el formato del dni
-        validator: (value) => {
-          return /^[0-9]{8}$/.test(value);
+
+    cliente: {
+        type: String,
+        required: true,
+        validate: {
+          // La regla de validación se usa para comprobar que el dni es válido
+          // Puede utilizar una expresión regular para comprobar el formato del dni
+          validator: (value) => {
+            return /^[0-9]{8}$/.test(value);
+          },
+          message: "El dni debe tener 8 dígitos",
         },
-        message: "El dni debe tener 8 dígitos",
       },
-    },
     nombreCliente:{
       type:String,
     },
+    
     apellidoCliente:{
       type:String,
     },
-  cancha:{
-      type: Schema.Types.String,
+    
+    cancha:{
+        type: Schema.Types.String,
+        upperCase: true,
+    },
+    
+    estado_pago:{
+      type: String, 
+      required: true,
       upperCase: true,
     },
-  estado_pago:{
-    type: String, 
-    required: true,
-    upperCase: true,
-   },
-  monto_cancha: {
-    type: Number,
-    required: true,
-  },
-  monto_sena: {
+    
+    monto_cancha: {
       type: Number,
       required: true,
-  },
-  fechaCopia:{
-    type: String,
-  },
-  fecha: {
-    type: Date, 
-    // required: true,
-    format: "YYYY-MM-DD"
     },
+    monto_sena: {
+      type: Number,
+      required: true,
+    },
+
+    fechaCopia:{
+      type: String,
+    },
+    
+    fecha: {
+      type: Date, 
+      required: true,
+      format: "YYYY-MM-DD"
+      },
+
     hora:{
       type: String, 
       required: true
-  },
-  
-  forma_pago:{
-    type: String,
-    required: true,
-    upperCase: true,
-  },
-  observacion: {
-    type: String,
-   },
-   user: {
-    type: String,
-    required: true
-  },
-  configuracion: {
-    type: Schema.Types.String,
-    ref: "Configuracion",
-}, 
- 
+    },
+    
+    forma_pago:{
+      type: String,
+      required: true,
+      upperCase: true,
+    },
+
+    observacion: {
+      type: String,
+    },
+
+    user: {
+      type: String,
+      required: true
+    },
+
+    configuracion: {
+      type: Schema.Types.String,
+      ref: "Configuracion",
+  }, 
 });
 
 ReservaSchema.method('toJSON', function(){
